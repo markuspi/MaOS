d05a10c247ddf45c679ed7703d5cebe5a49051cf
Markus Pielmeier <markuspi@users.noreply.github.com>
2021-05-01 21:18:20 +0200

    Add C++ Support

---
+++ libc/cpp/new.cpp
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+#include "stdlib"
+
+void *operator new(size_t size) {
+    return malloc(size);
+}
+
+void *operator new[](size_t size) {
+    return malloc(size);
+}
+
+void operator delete(void *p) {
+    free(p);
+}
+
+void operator delete[](void *p) {
+    free(p);
+}
